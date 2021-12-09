4959afb9a1dd056b146ff366f9696f6bacd4292e
Dietmar19 <dietmar19@yahoo.de>
2021-12-09 21:37:46 +0100

    Teständerung1

--- first class.py
+++ first class.py
@@ -7,9 +7,10 @@ Datum: 09.12.2021
 
 class Mensch:
     def __init__(self,name,alter,größe,):
-        self.__Name=name
+        self.Name=name
         self.Alter=alter
         self.Größe=größe
 mensch1=Mensch("Dietmar",63,1.86)
 print(mensch1.Name)
+print(mensch1.Alter)
 
