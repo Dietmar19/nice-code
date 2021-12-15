563fcd355e0ad0a0d998fa904098aa40b5a06bed
Dietmar19 <dietmar19@yahoo.de>
2021-12-15 20:16:15 +0100

    Änderung GUI 15.12.2021

---
+++ GUI.py
@@ -0,0 +1,22 @@
+"""
+Author: Dietmar
+Datum: 11.12.2021
+"""
+
+
+from tkinter import*   # import alle Funktionen des Moduls
+window = Tk()            # erstellt ein Fenster
+
+window.geometry('500x200') # setzt 200Pixel hoch, 00 Pixel breit
+
+
+window.title("GUI erster Versuch")
+label = Label(window,text="Python Tkinter")
+label.pack(expand=1)  # aktivierung des Labels
+
+frame=Frame(window,relief="ridge",borderwidth="20")
+
+frame.pack(fill="both", expand=1)
+
+
+window.mainloop()      # öffnet das Objekt Fenster
