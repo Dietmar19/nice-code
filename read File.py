bf64ad0cdcb75ae94b46ab095be83d8728a7142d
Dietmar19 <dietmar19@yahoo.de>
2021-12-21 16:55:34 +0100

    Test5

--- read File.py
+++ read File.py
@@ -7,21 +7,31 @@ Date: 21.12.2021
 dic= {"Name": "Baumann", "Vorname" : "Dietmar", "Age": 63, "Größe": 1.86}
 print(dic["Age"])
 """
+import os
+
+
+
 country_dict = {}
-filename = "Country.txt"
+filename = "C:\\Users\\Dietmar\\OneDrive\\Documents\\Repository Python\\nice-code\\Country.txt"
+print(os.getcwd())
 textfile = open(filename, "r")
    
 
 for line in textfile:
-    assign = line.split( "")
+    assign = line.split( ";")
     country_dict[assign[0]] = assign[1]
 textfile.close()
 
-while True:
+value = 1
+while value < 3:
+   
     country = input("Geben sie bitte ein Land ein : ")
+    
     if country in country_dict:
         print("Das deutsche Wort lautet: ", country_dict[country])
     else:
-        print("Das Land kann leider nicht gefunden werden")
-
+        print("Das Land kann leider nicht gefunden werden")  
+        print(value)
+    value+=1
+       
         
